8e6cca5b2a68e6f8bdbb650a595e5df7111dc753
Juanma Gomez <juanma.gomez.martin@gmail.com>
2024-05-23 13:45:51 +0200

    ft_isalnum ok

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -3,60 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: naima <naima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 11:52:48 by juagomez          #+#    #+#             */
-/*   Updated: 2024/04/23 12:18:46 by juagomez         ###   ########.fr       */
+/*   Updated: 2024/05/23 13:45:24 by naima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include <unistd.h>
+
+#include "libft.h"
 
 /* static void ft_putchar(char caracter)
 {
     write(1, &caracter, 1);
 } */
 
-static int  ft_isdigit(int caracter)
+int	ft_isalnum(int caract)
 {
-    unsigned int   is_digit;
+	unsigned int	is_alpha;
+	unsigned int	is_digit;
+	unsigned int	is_alnum;
 
-    is_digit = 0;
-    if (caracter >= '0' && caracter <= '9')
-    {
-        is_digit = 1;
-    }
-    return (is_digit);
-}
-
-static  int ft_isalpha(int caracter)
-{
-    unsigned int    is_alpha;
-
-    is_alpha = 0;
-    if ((caracter >= 'A' && caracter <= 'Z') || (caracter >= 'a' && caracter <= 'z'))
-    {
-        is_alpha = 1;
-    }
-    return (is_alpha);
-}
-
-
-int ft_isalnum(int caracter)
-{
-    unsigned int    is_alpha;
-    unsigned int    is_digit;
-    unsigned int    is_alnum;
-
-    is_alpha = ft_isalpha(caracter);
-    is_digit = ft_isdigit(caracter);
-
-    if(is_alpha == 1 || is_digit == 1)
-        is_alnum = 1;
-    else
-        is_alnum = 0;
-
-    return (is_alnum);
+	is_alpha = 0;
+	is_digit = 0;
+	is_alnum = 0;
+	if (caract >= '0' && caract <= '9')
+		is_digit = 1;
+	if ((caract >= 'A' && caract <= 'Z') || (caract >= 'a' && caract <= 'z'))
+		is_alpha = 1;
+	if (is_alpha == 1 || is_digit == 1)
+		is_alnum = 1;
+	else
+		is_alnum = 0;
+	return (is_alnum);
 }
 
 // #include <ctype.h>

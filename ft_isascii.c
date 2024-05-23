e24bb6282894a26b17eb7e4cf063692f29b5082c
Juanma Gomez <juanma.gomez.martin@gmail.com>
2024-05-23 13:52:28 +0200

    ft_isascii ok

--- ft_isascii.c
+++ ft_isascii.c
@@ -3,34 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: naima <naima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 13:20:14 by juagomez          #+#    #+#             */
-/*   Updated: 2024/04/23 13:45:34 by juagomez         ###   ########.fr       */
+/*   Updated: 2024/05/23 13:51:43 by naima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 // # include <unistd.h>
 
 /* void    ft_putchar(char caracter)
 {
     write(1, &caracter, 1);
-}*/ 
+} */
 
-// retorna distinto 0 si caracter tiene valor octal entre 0 y 177 (decimal 000 y 127)
+/* retorna distinto 0 si caracter tiene 
+valor octal entre 0 y 177 (decimal 000 y 127) ---- */
 // tabla valores ascii de control + imprimibles
-int ft_isascii(int caracter)
+int	ft_isascii(int caracter)
 {
-    unsigned int    is_ascii;
-    is_ascii = 0;
+	unsigned int	is_ascii;
 
-    if(caracter >= 0 && caracter <= 127)
-    {
-        is_ascii = 1;
-    }
-    return (is_ascii);
+	is_ascii = 0;
+	if (caracter >= 0 && caracter <= 127)
+		is_ascii = 1;
+	return (is_ascii);
 }
-#include <ctype.h>
+// #include <ctype.h>
 
 // no consigo testear con caracter 'ñ'
 /* int main(void)

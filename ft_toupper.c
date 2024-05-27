acac9b518294cde6a7fec2291812b58a9b4736ab
Juanma Gomez <juanma.gomez.martin@gmail.com>
2024-05-27 20:32:03 +0200

    ft_toupper ok

---
+++ ft_toupper.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 20:17:04 by juagomez          #+#    #+#             */
+/*   Updated: 2024/05/27 20:28:59 by juagomez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_toupper(int character)
+{
+	if (character >= 'a' && character <= 'z')
+		return (character - 32);
+	return (character);
+}

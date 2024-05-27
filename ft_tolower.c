9c33bd66b6e6df12c0b0e8c736991e5522774a5b
Juanma Gomez <juanma.gomez.martin@gmail.com>
2024-05-27 20:37:39 +0200

    ft_tolower ok

---
+++ ft_tolower.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 20:33:17 by juagomez          #+#    #+#             */
+/*   Updated: 2024/05/27 20:36:35 by juagomez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_tolower(int character)
+{
+	if (character >= 'A' && character <= 'Z')
+		return (character + 32);
+	return (character);
+}

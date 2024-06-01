884236988baee579bd2952ffa849aede6183dd7e
Juanma Gomez <juanma.gomez.martin@gmail.com>
2024-06-01 22:16:31 +0200

    ft_putstr_fd

---
+++ ft_putstr_fd.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/01 22:01:26 by juagomez          #+#    #+#             */
+/*   Updated: 2024/06/01 22:16:02 by juagomez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* 
+Escribe la cadena s en el descriptor de archivo especificado fd. Primero 
+verifica si la cadena s no es nula y luego usa la llamada al sistema de 
+escritura para escribir la cadena completa en el descriptor de archivo. 
+El número de bytes a escribir se calcula mediante el uso de una función 
+auxiliar "str_len", ya que cada carácter requiere exactamente un byte. */
+
+void	ft_putstr_fd(char *str, int fd)
+{
+	int	index;
+
+	index = 0;
+	if (str)
+	{
+		while (str[index] != '\0')
+		{
+			write(fd, &str[index], 1);
+			index++;
+		}
+	}
+}
+
+/* int	main(void)
+{
+	int fd = open("text.txt", O_RDWR);
+	ft_putstr_fd("Hola Mundo", fd);
+	close(fd);
+	return (0);
+} */

1968a96a28a5d786781fad49fd7154bb8ceaba96
Juanma Gomez <juanma.gomez.martin@gmail.com>
2024-06-03 22:29:08 +0200

    ft_putstr_fd refactorizacion

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 22:01:26 by juagomez          #+#    #+#             */
-/*   Updated: 2024/06/01 22:16:02 by juagomez         ###   ########.fr       */
+/*   Updated: 2024/06/01 22:21:17 by juagomez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ verifica si la cadena s no es nula y luego usa la llamada al sistema de
 escritura para escribir la cadena completa en el descriptor de archivo. 
 El número de bytes a escribir se calcula mediante el uso de una función 
 auxiliar "str_len", ya que cada carácter requiere exactamente un byte. */
+
+/* La función supone que la operación de escritura será exitosa y no 
+incluye manejo explícito de errores. Es importante asegurarse de que el 
+descriptor del archivo sea válido y esté abierto para escritura antes 
+de llamar a esta función para evitar comportamientos inesperados o errores*/
 
 void	ft_putstr_fd(char *str, int fd)
 {
